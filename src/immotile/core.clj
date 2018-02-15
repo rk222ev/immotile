4a88759794e726e23e9377b61a4ad8953fc137bf
Robin Karlsson <robinpkarlsson@gmail.com>
2018-02-15 21:01:41 +0100

    Initial

---
+++ src/immotile/core.clj
@@ -0,0 +1,7 @@
+(ns immotile.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/immotile/core_test.clj
@@ -0,0 +1,7 @@
+(ns immotile.core-test
+  (:require [clojure.test :refer :all]
+            [immotile.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
