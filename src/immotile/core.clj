93c3f07557fa4ee8809d22e8b8e2fb3c05465b65
Robin Karlsson <robinpkarlsson@gmail.com>
2018-02-16 10:47:04 +0100

    Improve elisp creation

--- src/immotile/core.clj
+++ src/immotile/core.clj
@@ -1,44 +1,33 @@
 (ns immotile.core
   (:require
    [clojure.java.shell :as shell]
-   [clojure.java.io :as io]
-   [clojure.pprint :as pprint])
+   [clojure.java.io :as io])
   (:gen-class))
 
 
-(def config {})
-
-
-(defn ->quoted-str
-  [s]
-  (str "\"" s "\""))
-
-
 (def org-export-command
-  (pprint/cl-format false "~a"
-                    `("progn"
-                      ("org-html-export-as-html nil nil nil t nil")
-                      ("with-current-buffer" ~(->quoted-str "*Org HTML Export*")
-                        ("princ" ("org-no-properties" ("buffer-string")))))))
+  `(~'progn
+    (~'org-html-export-as-html nil nil nil ~'t nil)
+    (~'with-current-buffer "*Org HTML Export*"
+     (~'princ (~'org-no-properties (~'buffer-string))))))
 
+
+(defn elisp-sexp
+  [absolute-path]
+  (str
+   `(~'progn
+     (~'find-file ~absolute-path)
+     ~org-export-command)))
+
+
+(defn emacs-shell-command
+  [absolute-path]
+  ["emacs" "-batch" "-eval" (elisp-sexp absolute-path)])
 
 
 (defn org->html
   [path]
-  (let [absolute-path (.getAbsolutePath (io/file path))]
-    (shell/sh
-     "emacs"
-     "-batch" "-eval"
-     (pprint/cl-format false "~a"
-                       `("progn"
-                         ("find-file" ~(->quoted-str absolute-path))
-                         ~org-export-command)))))
-
-
-(comment
-  "Prints all files in test/resources"
-  (for [x (file-seq (io/file "./test/resources"))]
-    (println x)))
+  (apply shell/sh (emacs-shell-command (.getAbsolutePath (io/file path)))))
 
 
 (defn -main
@@ -48,7 +37,16 @@
 
 
 (comment
+  "Prints all files in test/resources"
+  (for [x (file-seq (io/file "./test/resources"))]
+    (println x)))
+
+
+(comment
+  (spit "hello.html" (:out (org->html "test/resources/test.org")))
+
   (org->html "test/resources/test.org")
 
+  (elisp-sexp "some/file/path")
 
   )
--- test/immotile/core_test.clj
+++ test/immotile/core_test.clj
@@ -2,6 +2,9 @@
   (:require [clojure.test :refer :all]
             [immotile.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def expected-elisp-exp
+  "(progn (find-file \"some/file/path\") (progn (org-html-export-as-html nil nil nil t nil) (with-current-buffer \"*Org HTML Export*\" (princ (org-no-properties (buffer-string))))))")
+
+(deftest elisp-exp
+  (testing "")
+  (is (= expected-elisp-exp (elisp-sexp "some/file/path"))))
