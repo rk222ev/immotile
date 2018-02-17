7b93550652cffc1b02ec9d9f724dd6cdbd7fbca3
Robin Karlsson <robinpkarlsson@gmail.com>
2018-02-17 14:53:56 +0100

    Serve generated files over HTTP

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [http-kit "2.2.0"]]
   :main ^:skip-aot immotile.core
+  :jvm-opts ["--add-modules" "java.xml.bind"]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/immotile/core.clj
+++ src/immotile/core.clj
@@ -3,17 +3,18 @@
    [clojure.java.shell :as shell]
    [clojure.java.io :as io]
    [clojure.string :as str]
-   [hiccup.core :refer [html]])
+   [compojure.core :refer [defroutes]]
+   [compojure.route :as route]
+   [hiccup.core :refer [html]]
+   [org.httpkit.server :as server])
   (:gen-class))
 
 ;;; Org ;;;
-
 (def org-export-command
   `(~'progn
     (~'org-html-export-as-html nil nil nil ~'t nil)
     (~'with-current-buffer "*Org HTML Export*"
      (~'princ (~'org-no-properties (~'buffer-string))))))
-
 
 (defn elisp-sexp
   [absolute-path]
@@ -23,9 +24,7 @@
      ~org-export-command)))
 
 
-(defn emacs-shell-command
-  [absolute-path]
-  ["emacs" "-batch" "-eval" (elisp-sexp absolute-path)])
+(defn emacs-shell-command [absolute-path] ["emacs" "-batch" "-eval" (elisp-sexp absolute-path)])
 
 
 (defn absolute-file-path [path] (.getAbsolutePath (io/file path)))
@@ -37,15 +36,18 @@
 
 
 ;;; Read a lispy HTML template and generate real HTML using the ORG file as body;;;
-
+;;; Embed org document inside the HTML template ;;;;
 (defn remove-newlines [s] (str/replace s #"\n" ""))
 
+
 (defn read-template-fn [path] (load-file path))
+
 
 (defn create-page-data
   [org-file]
   {:body (:out (org->html org-file))
    :title "Site title!"})
+
 
 (defn generate-page
   [template-fn data]
@@ -54,18 +56,37 @@
        (remove-newlines)
        (str "<!doctype html>")))
 
+
 (comment
-  (generate-page
-   (read-template-fn "resources/templates/default.clj")
-   (create-page-data "test/resources/test.org"))
+  (spit "out.html"
+        (generate-page
+         (read-template-fn "resources/templates/default.clj")
+         (create-page-data "test/resources/test.org")))
 
 )
 
-;;; Embed org document inside the HTML template ;;;;
 ;;; Handle shell errors ;;;
 ;;; Read all org-files in folder ;;;
 ;;; Support for org-mode options ;;;
 ;;; Serve generated HTML with webserver ;;;
+(defn serve-static [req]
+  (let [mime-types {".clj" "text/plain"
+                    ".mp4" "video/mp4"
+                    ".ogv" "video/ogg"}
+        strip-leading-slash (fn [s] (str/replace-first s "/" ""))
+        headers (if-let [mimetype (mime-types (re-find #"\..+$" (:uri req)))]
+                  {:headers {"Content-Type" mimetype}})
+        body (slurp (strip-leading-slash (:uri req)))]
+    {:body body :headers headers}))
+
+
+(comment
+  (serve-static {:uri "/out.html" :headers {}}))
+
+
+(defn run []
+  (server/run-server serve-static {:port 8080}))
+
 ;;; Watch source files for updates and regenerate if needed ;;;
 
 
