f66a451dc3c932cbdca2c1bc872ffcc738819558
Robin Karlsson <robinpkarlsson@gmail.com>
2018-02-27 15:46:10 +0100

    Add some classes for css styling

--- resources/templates/default.clj
+++ resources/templates/default.clj
@@ -1,12 +1,12 @@
 (fn
-  [data]
+  [{title :title body :body}]
   [:html
    [:head
     [:meta {:charset "UTF-8"}]
     [:meta {:http-equiv "x-ua-compatible" :content "ie=edge"}]
     [:meta {:name "description" :content ""}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=no"}]
-    [:title (:title data)]
+    [:title title]
 
      ;;         <link rel="manifest" href="site.webmanifest">
      ;;         <link rel="apple-touch-icon" href="icon.png">
@@ -17,13 +17,13 @@
 
      ;;         <link rel="stylesheet" href="css/normalize.css">
     ;;         <link rel="stylesheet" href="css/main.css">
-    ]
+    [:link {:rel :stylesheet :href "default.css"}]]
    [:body
-    [:div
-     [:p "Hello there"]
-     [:div
+    [:div {:id :wrap}
+     [:h1 title]
+     [:div {:id :navigation}
       [:ul
        [:li [:a {:href "/index.html"} "home"]]
        [:li [:a {:href "/hello.html"} "hello"]]]]
      [:div {:class "content"}]
-     (:body data)]]])
+     body]]])
