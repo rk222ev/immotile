dc52e91648e00f6c34c32bfee8ccba54fa00efb8
Robin Karlsson <robinpkarlsson@gmail.com>
2018-09-02 21:26:23 +0200

    Create basic public files when running init

--- src/immotile/init.clj
+++ src/immotile/init.clj
@@ -11,15 +11,21 @@
   (doseq [folder ["cljs" (str "cljs/" project-name) "pages" "posts" "public" "templates"]]
     (.mkdir (io/file project-name "src" folder))))
 
+(defn- seed-file-destination
+  [file]
+  (-> (.getCanonicalPath file)
+                       (str/split #"resources/seed/")
+                       (second)))
+
 (defn- process-seed-files
   [project-name files]
   (doseq [file files]
-    (let [src-project-path (-> (.getCanonicalPath file)
-                       (str/split #"resources/seed/")
-                       (second)
-                       (str/replace ns-placeholder project-name))
-          processed-seed (-> (slurp file) (str/replace ns-placeholder project-name))]
-      (spit (io/file project-name src-project-path) processed-seed))))
+    (let [processed-seed (-> (slurp file)
+                             (str/replace ns-placeholder project-name))
+          destination (-> (seed-file-destination file)
+                          (str/replace ns-placeholder project-name))
+          target (io/file project-name destination)]
+      (spit target processed-seed))))
 
 (defn- make-files
   [project-name]
@@ -31,9 +37,14 @@
 
 (defn- make-config-files
   [project-name]
-  (process-seed-files project-name
-                      (map (comp io/file io/resource #(str "seed/" %))
-                           ["config.edn" "deps.edn"])))
+  (let [files ["config.edn" "deps.edn"]]
+    (process-seed-files project-name
+                        (map (comp io/file io/resource #(str "seed/" %)) files))))
+
+(defn copy-public-files
+  [project-name]
+  (doseq [file (drop 1 (file-seq (io/file (io/resource "seed/public"))))]
+    (io/copy file (io/file project-name "src" (seed-file-destination file)))))
 
 (defn make-project
   [project-name]
@@ -41,4 +52,5 @@
   (make-folder (str project-name "/src"))
   (make-src-subfolders project-name)
   (make-files project-name)
-  (make-config-files project-name))
+  (make-config-files project-name)
+  (copy-public-files project-name))
